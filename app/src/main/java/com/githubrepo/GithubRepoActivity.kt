85e7c7ea56a25cad906ef6c59eb909f019a66595
Bayar Şahin Tekin <bayar.sahintekin@kletech.com>
2019-12-27 10:47:27 +0300

    Initial commit

---
+++ app/src/main/java/com/githubrepo/GithubRepoActivity.kt
@@ -0,0 +1,12 @@
+package com.githubrepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GithubRepoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_github_repo)
+    }
+}
